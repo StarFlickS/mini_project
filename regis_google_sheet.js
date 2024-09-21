8ceeb2a8d5bae0d007fbd99ec053425ea0f4aeb7
Aut <niti.puan@bumail.net>
2024-09-21 12:32:51 +0700

    add

---
+++ regis_google_sheet.js
@@ -0,0 +1,14 @@
+const scriptURL = "https://script.google.com/macros/s/AKfycbwQgcI2vgfZAR5vLMIE0b66JcAu-mMje-X_6b83Y-xUrWzNlAEZJie3D80dptIlrn95Tw/exec" 
+
+const form = document.forms['contact-form']
+
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+  .then(response => alert("Thank you! your form is submitted successfully." ))
+  .then(() => { window.location.reload(); })
+  .catch(error => console.error('Error!', error.message))
+})
+
+
